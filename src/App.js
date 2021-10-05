134ef51fed03832a33c35e6332e205a868025ca9
IqbalAnas <iqbalana99.ia@gmail.com>
2021-10-05 02:51:01 -0700

    Bitter Changes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
-import { Component } from "react";
+// import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+// import { Component } from "react";
 
-import CurrentLocation from "./Map";
-//<Switch />;
+import MapApp from "./Components/MapApp";
+// //<Switch />;
 
-export class App extends Component {
-  state = {
-    showingInfoWindow: false, // Hides or shows the InfoWindow
-    activeMarker: {}, // Shows the active marker upon click
-    selectedPlace: {}, // Shows the InfoWindow to the selected place upon a marker
-  };
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true,
-    });
-
-  onClose = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null,
-      });
-    }
-  };
-
-  render() {
-    return (
-      <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
-        <Marker onClick={this.onMarkerClick} name={"Current Location"} />
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow>
-      </CurrentLocation>
-    );
-  }
+function App() {
+  return (
+    <>
+      <MapApp />
+    </>
+  );
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyBFAs2DVLRKt_tbJinn7Ox213xfMCTYqX4",
-})(App);
+export default App;
---
+++ src/Components/MapApp.js
@@ -0,0 +1,49 @@
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Component } from "react";
+
+import CurrentLocation from "./Map";
+//<Switch />;
+
+export class MapApp extends Component {
+  state = {
+    showingInfoWindow: false, // Hides or shows the InfoWindow
+    activeMarker: {}, // Shows the active marker upon click
+    selectedPlace: {}, // Shows the InfoWindow to the selected place upon a marker
+  };
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+
+  onClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
+        <Marker onClick={this.onMarkerClick} name={"Current Location"} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBFAs2DVLRKt_tbJinn7Ox213xfMCTYqX4",
+})(MapApp);
